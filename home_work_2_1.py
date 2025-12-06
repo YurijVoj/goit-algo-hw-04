5f2773b95e964cc8207f2542d52720e5890728bf
YurijVoj <pastaandcereals@gmail.com>
2025-12-06 15:02:03 +0200

    Доопрацювання функції та ств об. Path

--- home_work_2_1.py
+++ home_work_2_1.py
@@ -1,15 +1,16 @@
 from pathlib import Path
 
 def total_salary(path):
+    file_path = Path(path) 
     try:
-        if path.exists():
+        if file_path.exists():
             total = 0 
             number_of_lines = 0
             with open(path, 'r', encoding='utf-8') as file:
                 for line in file:
                     parts = line.split(',')
-                    if len(parts) >= 2: 
-                        salary = float(parts[2])
+                    if len(parts) > 1: 
+                        salary = float(parts[1])
                         total += salary 
                         number_of_lines += 1             
             average_salary = total / number_of_lines
@@ -19,3 +20,4 @@ def total_salary(path):
         return print("Файл 'salary_file' не існує ") 
     except (ValueError,IOError):
                 return print("Помилка при обробці файлу") 
+    
